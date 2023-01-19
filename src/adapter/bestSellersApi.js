a0a482ab610fc48a7339bda7428f0217f373a9d4
classicpauldev <1337@classicpaul.dev>
2023-01-19 14:30:00 +0100

    feat: enhance error handling in best sellers API (#183)

--- src/adapter/bestSellersApi.js
+++ src/adapter/bestSellersApi.js
@@ -5,18 +5,31 @@ const BEST_SELLERS_ENDPOINT = `${baseUrl}api/v1/products/best-sellers`;
 export const bestSellersApi = {
   // Get best selling products
   getBestSellers: async () => {
-    const response = await fetch(BEST_SELLERS_ENDPOINT, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
+    try {
+      const response = await fetch(BEST_SELLERS_ENDPOINT, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
 
-    if (!response.ok) {
-      throw new Error(`Failed to fetch best sellers: ${response.statusText}`);
+      if (!response.ok) {
+        const errorData = await response.json().catch(() => ({}));
+        throw new Error(
+          errorData.message || 
+          `Failed to fetch best sellers: ${response.statusText}`
+        );
+      }
+
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      // Re-throw with more context if it's not already an Error
+      if (error instanceof Error) {
+        throw error;
+      }
+      throw new Error(`Network error: ${error.message || 'Unknown error'}`);
     }
-
-    return await response.json();
   },
 };
 
